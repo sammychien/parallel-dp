33d7d67f907561a58d80da8ad6e15b7b5ad38ddc
Sammy Chien <sammychien@utexas.edu>
2021-11-05 14:01:02 -0500

    Add additional method signature to wlis

--- weighted-least-interval-scheduling/main.cpp
+++ weighted-least-interval-scheduling/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <chrono>
 
-void weightedLISSeq() {}
-void weightedLISPar() {}
+void wlisSeq() {}
+void wlisParBF() {}
+void wlisParDP() {}
 
 int main() {
     using std::chrono::high_resolution_clock;
@@ -10,21 +11,26 @@ int main() {
     using std::chrono::milliseconds;
 
     auto t1 = high_resolution_clock::now();
-    weightedLISSeq();
+    wlisSeq();
     auto t2 = high_resolution_clock::now();
 
     auto t3 = high_resolution_clock::now();
-    weightedLISPar();
+    wlisParBF();
     auto t4 = high_resolution_clock::now();
+
+    auto t5 = high_resolution_clock::now();
+    wlisParDP();
+    auto t6 = high_resolution_clock::now();
 
     /* Getting number of milliseconds as an integer. */
     duration<double, std::milli> ms_double_seq = t2 - t1;
-
-    duration<double, std::milli> ms_double_par = t4 - t3;
+    duration<double, std::milli> ms_double_par_bf = t4 - t3;
+    duration<double, std::milli> ms_double_par_dp = t6 - t5;
 
 
     std::cout << ms_double_seq.count() << "ms\n";
-    std::cout << ms_double_par.count() << "ms\n";
+    std::cout << ms_double_par_bf.count() << "ms\n";
+    std::cout << ms_double_par_dp.count() << "ms\n";
 
     return 0;
 }
