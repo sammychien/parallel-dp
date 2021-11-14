ff08433627fedf8415bfe1182ae4dbe5aa4bf500
Sammy Chien <sammychien@utexas.edu>
2021-11-14 16:37:23 -0600

    Adding parallel algo without llp for wis

--- weighted-least-interval-scheduling/wlis.cpp
+++ weighted-least-interval-scheduling/wlis.cpp
@@ -66,6 +66,7 @@ class WLIS {
     bool sorted = false;
 
     void parseInput(std::string inputFileName) {
+        std::vector<Interval>().swap(this->inputIntervals); // Deallocate and clear vector
         std::ifstream infile;
         infile.open(inputFileName, std::ifstream::in);
 
@@ -95,21 +96,20 @@ class WLIS {
     }
 
     int wlisSeq() {
-        std::vector<Interval> intervals = this->inputIntervals;
-        int numIntervals = intervals.size();
+        int numIntervals = this->inputIntervals.size();
 
         // First, sort the intervals in ascending order of their finish times if not already sorted
         if (!sorted) {
-            std::sort (intervals.begin(), intervals.end());
+            std::sort (this->inputIntervals.begin(), this->inputIntervals.end());
             for (int i = 0; i < numIntervals; i++) {
-                intervals.at(i).setId(i);
+                this->inputIntervals.at(i).setId(i);
             }
         }
 
         // Next, calculate P(j), where P(j) is the rightmost interval i such that i and j are compatible (f_i <= s_j)
         int P [numIntervals];
-        for (auto &interval : intervals) {
-            P[interval.id] = getP(intervals, interval);
+        for (auto &interval : this->inputIntervals) {
+            P[interval.id] = getP(this->inputIntervals, interval);
         }
 
         // Then, use the recurrence equation: OPT(j) = max(weight(j) + OPT(P[j]), OPT(j-1))
@@ -117,7 +117,7 @@ class WLIS {
         std::unordered_map<int, std::vector<Interval>> OPT_path;
 
         for (int i = 0; i < numIntervals; i++) {
-            Interval currentInterval = intervals.at(i);
+            Interval currentInterval = this->inputIntervals.at(i);
             int with = currentInterval.weight + OPT[P[i]];
             int without = OPT[i-1];
             if (with > without) {
@@ -135,6 +135,52 @@ class WLIS {
     }
 
     int wlisPar() {
+        // Settings
+        int par_enabled = 1;
+
+        int numIntervals = this->inputIntervals.size();
+
+        // First, sort the intervals in ascending order of their finish times if not already sorted
+        if (!sorted) {
+            std::sort (this->inputIntervals.begin(), this->inputIntervals.end());
+            #pragma omp parallel for if(par_enabled)
+            for (int i = 0; i < numIntervals; i++) {
+                this->inputIntervals.at(i).setId(i);
+            }
+        }
+
+        // Next, calculate P(j), where P(j) is the rightmost interval i such that i and j are compatible (f_i <= s_j)
+        // We can do this in parallel
+        int P [numIntervals];
+        #pragma omp parallel for if(par_enabled)
+        for (int i = 0; i < numIntervals; i++) {
+            Interval interval = this->inputIntervals.at(i);
+            P[i] = getP(this->inputIntervals, interval);
+        }
+
+        // Then, use the recurrence equation: OPT(j) = max(weight(j) + OPT(P[j]), OPT(j-1))
+        std::unordered_map<int, int> OPT { {-1, 0}, {0, 0}, };
+        std::unordered_map<int, std::vector<Interval>> OPT_path;
+
+        for (int i = 0; i < numIntervals; i++) {
+            Interval currentInterval = this->inputIntervals.at(i);
+            int with = currentInterval.weight + OPT[P[i]];
+            int without = OPT[i-1];
+            if (with > without) {
+                // choose the current one
+                OPT_path[i] = OPT_path[P[i]];
+                OPT_path[i].push_back(currentInterval);
+                OPT[i] = with;
+            } else {
+                OPT_path[i] = OPT_path[i-1];
+                OPT[i] = without;
+            }
+        }
+
+        return OPT[numIntervals-1];
+    }
+
+    int wlisLLP() {
         // Settings
         int par_enabled = 1;
 
@@ -161,27 +207,24 @@ class WLIS {
 
         // Then, we use an LLP algorithm to find the global state G
         int G [numIntervals+1];
-        bool diffs [numIntervals];
 
         G[0] = 0;
         #pragma omp parallel for if(par_enabled)
         for (int i = 0; i < numIntervals; i++) {
             G[i+1] = 0;
-            diffs[i] = true;
         }
 
         bool diffExists = true;
+        int GCalc [numIntervals];
         while (diffExists) {
-            int GCalc [numIntervals];
+            diffExists = false;
             #pragma omp parallel if(par_enabled)
             {
                 #pragma omp for
                 for (int i = 0; i < numIntervals; i++) {
                     GCalc[i] = std::max(this->inputIntervals.at(i).weight + G[P[i]+1], G[i]);
                     if (GCalc[i] != G[i+1]) {
-                        diffs[i] = true;
-                    } else {
-                        diffs[i] = false;
+                        diffExists = true;
                     }
                 }
 
@@ -190,16 +233,7 @@ class WLIS {
                     G[i+1] = GCalc[i];
                 }
             }
-
-            diffExists = false;
-            #pragma omp parallel for
-            for (int i = 0; i < numIntervals; i++) {
-                if (diffs[i]) {
-                    diffExists = true;
-                }
-            }
         }
-
         return G[numIntervals];
     }
 
@@ -223,6 +257,8 @@ public:
             return wlisSeq();
         } else if (algoType == "par") {
             return wlisPar();
+        } else if (algoType == "parllp") {
+            return wlisLLP();
         } else {
             // Invalid algoType, print error
             return -1;
@@ -238,31 +274,38 @@ int main(int argc, char *argv[]) {
     using std::chrono::milliseconds;
     std::cout.precision(8);
 
-    int numTrials = 1;
+    int numTrials = 3;
     for (int i = 1; i < argc; i++) {
         WLIS wlis;
-        wlis.init(argv[i], true);
 
         std::cout << "-----------------------------------" << std::endl;
         std::cout << "WLIS for test: " << argv[i] << std::endl;
         for (int trial = 1; trial <= numTrials; trial++) {
 
+            wlis.init(argv[i], true);
             auto t1 = high_resolution_clock::now();
             int seqResult = wlis.run("seq");
             auto t2 = high_resolution_clock::now();
 
+            wlis.init(argv[i], true);
             auto t3 = high_resolution_clock::now();
             int parResult = wlis.run("par");
             auto t4 = high_resolution_clock::now();
 
+            wlis.init(argv[i], true);
+            auto t5 = high_resolution_clock::now();
+            int parllpResult = wlis.run("parllp");
+            auto t6 = high_resolution_clock::now();
+
             /* Getting number of milliseconds as an integer. */
             duration<double, std::milli> ms_double_seq = t2 - t1;
             duration<double, std::milli> ms_double_par = t4 - t3;
+            duration<double, std::milli> ms_double_parllp = t6 - t5;
 
-            if (seqResult != parResult) {
-                std::cout << "Incorrect results: Seq: " << std::to_string(seqResult) << " Par: " << std::to_string(parResult) << std::endl;
+            if (seqResult == parResult && parResult == parllpResult) {
+                std::cout << std::fixed << ms_double_seq.count() << "\t" << ms_double_par.count() << "\t" << ms_double_parllp.count() << std::endl;
             } else {
-                std::cout << std::fixed << ms_double_seq.count() << "\t" << ms_double_par.count() << std::endl;
+                std::cout << "Incorrect results: Seq: " << std::to_string(seqResult) << " Par: " << std::to_string(parResult) << " ParLLP: " << std::to_string(parllpResult) << std::endl;
             }
         }
 
